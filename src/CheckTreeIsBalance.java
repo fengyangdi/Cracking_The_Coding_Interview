abf870128ace06997417f84e982c76b5aa2b5cd7
GGM <fengyangdi@gmail.com>
2016-05-08 21:14:26 +0800

    检查树的平衡性

---
+++ src/CheckTreeIsBalance.java
@@ -0,0 +1,38 @@
+/**
+ * Created by GGM on 2016/4/29.
+ * 判断树是否平衡
+ */
+public class CheckTreeIsBalance {
+    /**
+     * 判断树是否平衡
+     * @param root 树的根
+     * @param <T> 泛型参数
+     * @return 是否平衡
+     */
+    public static <T> boolean isBalance(Tree<T> root){
+        ReTurnType re = isBalanceRecursion(root);
+        return re.isBalance;
+    }
+
+    private static <T> ReTurnType isBalanceRecursion(Tree<T> root) {
+        if (root == null) return new ReTurnType(true,0);
+        ReTurnType left = isBalanceRecursion(root.left);
+        ReTurnType right = isBalanceRecursion(root.rigth);
+        int high = left.high>right.high?left.high:right.high;
+        if (!left.isBalance || right.isBalance) return new ReTurnType(false,high+1);
+        else {
+            boolean isBalance = Math.abs(left.high-right.high) <= 1;
+            return new ReTurnType(isBalance,high+1);
+        }
+    }
+
+    private  static  class ReTurnType{
+        boolean isBalance;
+        int high;
+
+        public ReTurnType(boolean _isBalance, int _high){
+            this.isBalance = _isBalance;
+            this.high = _high;
+        }
+    }
+}
---
+++ src/Tree.java
@@ -0,0 +1,9 @@
+/**
+ * Created by GGM on 2016/4/29.
+ * 树结构的简单定义
+ */
+public class Tree<T> {
+    T val;
+    Tree left;
+    Tree rigth;
+}
